92cf6ca4403c060af58cae857ff88c09d29a22c6
pduchesne <pduchesne@gmail.com>
2013-11-02 11:52:20 +0100

    - add tagcloud

--- ckanext/odvl/theme/public/js/tag_cloud.js
+++ ckanext/odvl/theme/public/js/tag_cloud.js
@@ -5,12 +5,23 @@ this.ckan.module('tag-cloud', function ($$, _) {
 
     initialize: function () {
         var $el = this.el
+        var counts = []
         $el.find(".tag").each(
             function(idx, tag) {
-                var rel=$$(tag).attr("data-count")/$el.attr("data-totalcount")
-                $$(tag).css("font-size", rel*100+"%")
-
+                counts.push($$(tag).attr("data-count"))
             })
+        var min = Math.min.apply(null,counts)
+        var max = Math.max.apply(null,counts)
+        var delta = max - min
+
+        $el.find(".tag").each(
+            function(idx, tag) {
+                var rel = delta == 0 ? 100 : 60 + 80 * ($$(tag).attr("data-count") - min) / delta
+                $$(tag).css("font-size", rel+"%")
+                       .css("border-radius", 15*rel/100+"px")
+            })
+        /$el.attr("data-totalcount")
+
     }
 
   }
