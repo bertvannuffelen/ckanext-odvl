4a2b317ffcce80eab82039b3857d10b8b6222dbe
pduchesne <pduchesne@gmail.com>
2015-05-22 18:07:56 +0200

    add api action to retrieve user roles in organizations

--- ckanext/odvl/plugin.py
+++ ckanext/odvl/plugin.py
@@ -2,11 +2,67 @@ import os
 import inspect
 import ckan.plugins as p
 import ckanext.odvl.helpers as helpers
+import ckan.logic as logic
+import ckan.new_authz as new_authz
+import ckan.lib.dictization.model_dictize as model_dictize
+from ckan import model
+
+
+@logic.side_effect_free
+def members_of_orgs(context, data_dict=None):
+
+    model = context['model']
+    user = context['user']
+
+    #logic.check_access('members_of_orgs', context, data_dict)
+    sysadmin = new_authz.is_sysadmin(user)
+
+    if sysadmin:
+        users = model.Session.query(model.User) \
+            .filter(model.User.state == 'active')
+
+        members = model.Session.query(model.Member) \
+            .filter(model.Member.table_name == 'user') \
+            .filter(model.Member.capacity.isnot(None)) \
+            .filter(model.Member.state == 'active')
+
+        user_list = []
+
+        for user in users.all():
+            members = model.Session.query(model.Member) \
+                .filter(model.Member.table_name == 'user') \
+                .filter(model.Member.capacity.isnot(None)) \
+                .filter(model.Member.table_id == user.id) \
+                .filter(model.Member.state == 'active').all()
+            if (len(members)>0):
+                user_dict = {
+                    'name' : user.name,
+                    'fullname' : user.fullname,
+                    'email' : user.email}
+                user_dict['orgs'] = []
+                for role in members:
+                    org = model.Session.query(model.Group.name)\
+                        .filter(model.Group.is_organization == True) \
+                        .filter(model.Group.id == role.group_id) \
+                        .filter(model.Group.state == 'active')\
+                        .first()
+                    user_dict['orgs'].append(
+                        {'role' : role.capacity,
+                         'org_name' : org.name})
+                user_list.append(user_dict)
+        return user_list
 
 
 class ODVLExtension(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.ITemplateHelpers)
+    p.implements(p.IActions)
+
+
+    def get_actions(self):
+        return {
+            'members_of_orgs' : members_of_orgs
+        }
 
     def update_config(self, config):
 
