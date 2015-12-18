9635b7d0158c7dbb2f716fd93262f964adbccbcd
pduchesne <pduchesne@gmail.com>
2015-12-18 16:09:03 +0100

    fix tag cloud for new css style

--- ckanext/odvl/theme/public/js/tag_cloud.js
+++ ckanext/odvl/theme/public/js/tag_cloud.js
@@ -16,9 +16,11 @@ this.ckan.module('tag-cloud', function ($$, _) {
 
         $el.find(".tag").each(
             function(idx, tag) {
-                var rel = delta == 0 ? 100 : 75 + 50 * ($$(tag).attr("data-count") - min) / delta
-                $$(tag).css("font-size", rel+"%")
+                var relPos = delta == 0 ? 0.5 : ($$(tag).attr("data-count") - min) / delta
+                var relSize = 75 + 50 * relPos
+                //$$(tag).css("font-size", relSize+"%")
                        //.css("border-radius", 15*rel/100+"px")
+                $$(tag).parent().toggleClass('tags-list-item__size-'+ (Math.round(relPos * 3)+1), true)
             })
         /$el.attr("data-totalcount")
 
